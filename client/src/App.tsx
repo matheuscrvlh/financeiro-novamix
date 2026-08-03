f370a97da0cd3f66dba2ed1e1c7efde30e7c0ada
Matheus Carvalho <matheusscarvlh@gmail.com>
2026-08-02 21:16:19 -0300

    feat: adiciona docker e frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from 'react-router-dom'
+
+export default function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={ }/>
+      </Routes>
+    </>
+  )
+}
