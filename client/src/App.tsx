2d8fc876b36fea3e19cef704eaa310e528238131
Matheus Carvalho <matheusscarvlh@gmail.com>
2026-08-02 21:59:45 -0300

    fix: frontend para deploy

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={ }/>
+        <Route path='/' element={<div>Financeiro Novamix</div>}/>
       </Routes>
     </>
   )
